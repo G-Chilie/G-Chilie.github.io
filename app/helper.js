256cd29f0f78c660293d58f237eacb6fbcaab37a
G-Chilie <okonkwolinda7@gmail.com>
2019-01-01 18:24:03 +0100

    add function getNewId

---
+++ app/helper.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const logger = require('../lib/logger');
+
+const getNewId = (array) => {
+  if (array.length > 0) {
+    return array[array.length - 1].id + 1;
+  } return 1;
+};
