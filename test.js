1f111d1e8503c52b5d8dc85dedb6ae588a71f6fe
G-Chilie <okonkwolinda7@gmail.com>
2019-01-02 23:31:01 +0100

    Add test for POST /api/v1/meetups' route

--- test.js
+++ test.js
@@ -62,4 +62,54 @@ describe('Meetup', () => { // eslint-disable-line on-undef
     });
   });
 
+  describe('POST /api/v1/meetups', () => { // eslint-disable-line no-undef
+    const fields = ['title', 'location', 'happeningOn', 'tags', 'images'];
+    it('should create a new meetup record', (done) => { // eslint-disable-line no-undef
+      const meetup = {
+        id: 4,
+        createdOn: "Mon Dec 31 2018 20:28:47 GMT+0100 (WAT)",
+        title: "ConcatenateConf",
+        location: "Yaba, Lagos",
+        happeningOn: "Wed Dec 26 2018 08:18:30 GMT+0100 (WAT)",
+        tags: ["lorem", "lorem"],
+        images: ["images1", "images2"],
+      };
+      chai.request(app)
+        .post('/api/v1/meetups')
+        .send(meetup)
+        .end((err, res) => {
+          res.status.should.equal(200);
+          res.body.status.should.equal(201);
+          res.body.should.be.a('object');
+          res.body.data.should.have.lengthOf(1);
+          done();
+        });
+    });
+
+    fields.forEach(field => {
+      it(`should return a validation error message if ${field} is undefined`, (done) => {
+        const meetup = {
+          id: 4,
+          createdOn: "Mon Dec 31 2018 20:28:47 GMT+0100 (WAT)",
+          title: "ConcatenateConf",
+          location: "Yaba, Lagos",
+          happeningOn: "Wed Dec 26 2018 08:18:30 GMT+0100 (WAT)",
+          tags: ["lorem", "lorem"],
+          images: ["images1", "images2"],
+        };
+        meetup[field] = undefined;
+        chai.request(app)
+          .post('/api/v1/meetups')
+          .send(meetup)
+          .end((err, res) => {
+            res.status.should.equal(200);
+            res.body.status.should.equal(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message');
+            done();
+          })
+      });
+    });
+  });
+
 });
