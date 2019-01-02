4a95ba9403f2345bbf4620a2d3a4d3b079d74cd2
G-Chilie <okonkwolinda7@gmail.com>
2019-01-02 23:23:38 +0100

    Add test for GET /api/v1/meetups route

--- test.js
+++ test.js
@@ -1,33 +1,24 @@
+// Import the dependencies for testing
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./server');
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
 
-const { expect } = chai;
+describe('Meetup', () => { // eslint-disable-line on-undef
+  describe('GET /api/v1/meetups', () => { // eslint-disable-line no-undef
+    it('should return all meetup record', (done) => { // eslint-disable-line no-undef
+      chai.request(app)
+        .get('/api/v1/meetups')
+        .end((err, res) => {
+          res.status.should.equal(200);
+          res.body.status.should.equal(200);
+          res.body.should.be.a('object');
+          res.body.data.should.not.have.lengthOf(0);
+          done();
+        });
+    });
+  });
 
-describe('Fisrt Test', () => {
-  it('should return a string', () => {
-    expect('ci test travis').to.equal('ci test travis');
-  });
-  it('should return a number', () => {
-    expect('ci test travis').to.equal('ci test travis');
-  });
-  it('should return a array', () => {
-    expect('ci test travis').to.equal('ci test travis');
-  });
-  it('should return a object', () => {
-    expect('ci test travis').to.equal('ci test travis');
-  });
-});
-
-describe('second Test', () => {
-  it('should return a string', () => {
-    expect('ci test travis').to.equal('ci test travis');
-  });
-  it('should return a number', () => {
-    expect('ci test travis').to.equal('ci test travis');
-  });
-  it('should return a array', () => {
-    expect('ci test travis').to.equal('ci test travis');
-  });
-  it('should return a object', () => {
-    expect('ci test travis').to.equal('ci test travis');
-  });
 });
