bf543d7befed8d3b24d11fca2bfc73a05ba8f575
G-Chilie <okonkwolinda7@gmail.com>
2019-01-02 23:25:33 +0100

    Add test for GET /api/v1/meetups/:id route

--- test.js
+++ test.js
@@ -21,4 +21,33 @@ describe('Meetup', () => { // eslint-disable-line on-undef
     });
   });
 
+  describe('GET /api/v1/meetups/:id', () => { // eslint-disable-line no-undef
+    it('should get a single meetup record', (done) => { // eslint-disable-line no-undef
+      const id = 1;
+      chai.request(app)
+        .get(`/api/v1/meetups/${id}`)
+        .end((err, res) => {
+          res.status.should.equal(200);
+          res.body.status.should.equal(200);
+          res.body.should.be.a('object');
+          res.body.data.should.have.lengthOf(1);
+          done();
+        });
+    });
+
+    it('should return error message for non-existing meetup', (done) => { // eslint-disable-line no-undef
+      const id = 10000;
+      chai.request(app)
+        .get(`/api/v1/meetups/${id}`)
+        .end((err, res) => {
+          res.status.should.equal(200);
+          res.body.status.should.equal(404);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message');
+          done();
+        });
+    });
+  });
+
+
 });
