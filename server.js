f55569c87b9aae37516662121c606f3f44f73ff2
G-Chilie <okonkwolinda7@gmail.com>
2019-01-06 15:47:13 +0100

    Add API-documentation

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const logger = require('./app/lib/logger');
 const routes = require('./app/routes/index');
+const swaggerUi = require('swagger-ui-express');
+
+let docs = require('./swagger.json');;
+
 
 const app = express();
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '8000';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(docs));
+
 
 if (!module.parent) {
   app.listen(port, () => {
