462fd5693568bd0eb7d5e6a9ffcbe945b448ab0d
G-Chilie <okonkwolinda7@gmail.com>
2019-01-10 21:16:12 +0100

    Add node_modules to gitgnore

---
+++ dist/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var express = require('express');
+var swaggerUi = require('swagger-ui-express');
+var logger = require('./app/lib/logger');
+var routes = require('./app/routes/index');
+
+var docs = require('./swagger.json');
+
+var app = express();
+var port = process.env.PORT || '3000';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(docs));
+
+if (!module.parent) {
+  app.listen(port, function () {
+    logger.info('app running at port ' + port);
+  });
+}
+
+module.exports = app;
