e608d753ef97120b9afb654db6224f8eb1090c41
G-Chilie <okonkwolinda7@gmail.com>
2019-01-11 03:13:45 +0100

    initial commit on convering ES5 to ES6

--- dist/server/models/meetup.js
+++ dist/server/models/meetup.js
@@ -1,35 +1,46 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.rsvpMeetup = exports.getUpcomingMeetups = exports.getMeetup = exports.getAllMeetups = exports.insertMeetup = undefined;
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var path = require('path');
+var _path = require('path');
 
-var filename = path.join(__dirname, '../data/meetup.json');
+var _path2 = _interopRequireDefault(_path);
 
-var meetups = require('../data/meetup.json');
+var _meetup = require('../data/meetup.json');
 
-var helper = require('../lib/helper.js');
+var _meetup2 = _interopRequireDefault(_meetup);
 
-// const logger = require('../lib/logger');
+var _helper = require('../lib/helper.js');
 
-var jsonFilename = path.join(__dirname, '../data/rsvp.json');
+var _helper2 = _interopRequireDefault(_helper);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var filename = _path2.default.join(__dirname, '../data/meetup.json');
+
+var jsonFilename = _path2.default.join(__dirname, '../data/rsvp.json');
 
 var rsvps = require('../data/rsvp.json');
 
 function insertMeetup(newMeetup) {
   return new Promise(function (resolve) {
-    var id = helper.getNewId(meetups);
-    var createdOn = helper.newDate();
+    var id = _helper2.default.getNewId(_meetup2.default);
+    var createdOn = _helper2.default.newDate();
     var meetup = _extends({ id: id, createdOn: createdOn }, newMeetup);
-    meetups.push(meetup);
-    helper.writeJSONFile(filename, meetups);
+    _meetup2.default.push(meetup);
+    _helper2.default.writeJSONFile(filename, _meetup2.default);
     resolve(meetup);
   });
 }
 
 function getMeetup(id) {
   return new Promise(function (resolve, reject) {
-    helper.mustBeInArray(meetups, parseInt(id, 10)).then(function (meetup) {
+    _helper2.default.mustBeInArray(_meetup2.default, parseInt(id, 10)).then(function (meetup) {
       return resolve(meetup);
     }).catch(function (err) {
       return reject(err);
@@ -39,27 +50,27 @@ function getMeetup(id) {
 
 function getAllMeetups() {
   return new Promise(function (resolve) {
-    if (meetups.length === 0) {
+    if (_meetup2.default.length === 0) {
       resolve({ // eslint-disable-line prefer-promise-reject-errors
         status: 204,
         data: []
       });
       return;
     }
-    resolve(meetups);
+    resolve(_meetup2.default);
   });
 }
 
 function getUpcomingMeetups() {
   return new Promise(function (resolve, reject) {
-    if (meetups.length === 0) {
+    if (_meetup2.default.length === 0) {
       reject({ // eslint-disable-line prefer-promise-reject-errors
         status: 204,
         data: []
       });
     }
-    var upcomingMeetups = meetups.filter(function (meetup) {
-      return meetup.happeningOn > helper.newDate();
+    var upcomingMeetups = _meetup2.default.filter(function (meetup) {
+      return meetup.happeningOn > _helper2.default.newDate();
     });
     if (upcomingMeetups) {
       resolve(upcomingMeetups);
@@ -74,8 +85,8 @@ function getUpcomingMeetups() {
 
 function rsvpMeetup(meetupId) {
   return new Promise(function (resolve, reject) {
-    helper.mustBeInArray(meetups, parseInt(meetupId, 10)).then(function (meetup) {
-      var id = helper.getNewId(rsvps);
+    _helper2.default.mustBeInArray(_meetup2.default, parseInt(meetupId, 10)).then(function (meetup) {
+      var id = _helper2.default.getNewId(rsvps);
       var title = meetup.topic;
       var rsvpDefaults = {
         meetup: meetupId,
@@ -85,17 +96,15 @@ function rsvpMeetup(meetupId) {
       };
       var rsvp = _extends({ id: id, title: title }, rsvpDefaults);
       rsvps.push(rsvp);
-      helper.writeJSONFile(jsonFilename, rsvps);
+      _helper2.default.writeJSONFile(jsonFilename, rsvps);
       resolve(rsvp);
     }).catch(function (err) {
       return reject(err);
     });
   });
 }
-module.exports = {
-  insertMeetup: insertMeetup,
-  getAllMeetups: getAllMeetups,
-  getMeetup: getMeetup,
-  getUpcomingMeetups: getUpcomingMeetups,
-  rsvpMeetup: rsvpMeetup
-};
+exports.insertMeetup = insertMeetup;
+exports.getAllMeetups = getAllMeetups;
+exports.getMeetup = getMeetup;
+exports.getUpcomingMeetups = getUpcomingMeetups;
+exports.rsvpMeetup = rsvpMeetup;
